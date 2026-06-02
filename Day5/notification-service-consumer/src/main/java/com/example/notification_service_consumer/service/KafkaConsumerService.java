9fab8c1fb7049ec67ef6f5aceef0cc5b5e077eb2
RinshaRinz <rinz.nzrn@gmail.com>
2026-06-02 09:04:24 +0530

    Adding notification consumer

---
+++ Day5/notification-service-consumer/src/main/java/com/example/notification_service_consumer/service/KafkaConsumerService.java
@@ -0,0 +1,17 @@
+package com.example.notification_service_consumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(
+            topics = "orders-topic",
+            groupId = "notification-group"
+    )
+    public void consume(String message){
+
+        System.out.println("Notification Service Received : " + message);
+    }
+}
