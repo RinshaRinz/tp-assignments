5cd9ea55099be745c59c04756e3756a561248674
RinshaRinz <rinz.nzrn@gmail.com>
2026-06-02 07:53:30 +0530

    Kafka Assignments

---
+++ Day5/order-service-producer/src/main/java/com/example/order_service_producer/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.order_service_producer.controller;
+
+import com.example.order_service_producer.dto.OrderDto;
+import com.example.order_service_producer.service.OrderProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderProducerService producer;
+
+    @PostMapping
+    public String createOrder(@RequestBody OrderDto order){
+        producer.publishOrder(order);
+        producer.publishStatus("ORDER_CREATED");
+        return "Order Event Published";
+    }
+
+}
---
+++ Day5/order-service-producer/src/main/java/com/example/order_service_producer/service/OrderProducerService.java
@@ -0,0 +1,23 @@
+package com.example.order_service_producer.service;
+
+import com.example.order_service_producer.dto.OrderDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishOrder(OrderDto order){
+        kafkaTemplate.send("order-events", order);
+    }
+
+    public void publishStatus(String status){
+        kafkaTemplate.send("order-status", status);
+    }
+
+
+}
